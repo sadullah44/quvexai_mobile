b583588b98fdbcaf1c5469179db3620a7886706c
Nazlhnm <nazlikeles5050@gmail.com>
2025-10-31 17:12:52 +0300

    fix: convert core files to folders (theme, storage, network)

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF4A90E2);
+  static const Color secondaryColor = Color(0xFF50E3C2);
+
+  static final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: primaryColor,
+    fontFamily: 'Poppins',
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(color: primaryColor, elevation: 0),
+    colorScheme: ColorScheme.light(
+      primary: primaryColor,
+      secondary: secondaryColor,
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: primaryColor,
+    fontFamily: 'Poppins',
+    scaffoldBackgroundColor: const Color(0xFF121212),
+    appBarTheme: const AppBarTheme(color: Colors.black),
+    colorScheme: ColorScheme.dark(
+      primary: primaryColor,
+      secondary: secondaryColor,
+    ),
+  );
+}
